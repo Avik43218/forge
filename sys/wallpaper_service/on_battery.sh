d189f1067b461bdc5b8e82ec9e715dcdbcbcc7d6
Avik43218 <roychoudhuryavik005@gmail.com>
2026-04-01 19:22:35 +0530

    Simplified dynamic wallpaper switching pipeline

--- sys/wallpaper_service/on_battery.sh
+++ sys/wallpaper_service/on_battery.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
+CURR_IMAGE=$(grep -Po '(?<=Image=)file://.*' ~/.config/plasma-org.kde.plasma.desktop-appletsrc | head -n 1)
+
 export USER_ID=$(id -u)
 export XDG_RUNTIME_DIR="/run/user/$USER_ID"
 export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
 
-dbus-send --session --dest=org.kde.plasmashell --type=method_call \
-/PlasmaShell org.kde.PlasmaShell.evaluateScript \
-"string:
-var allDesktops = desktops();
-for (var i = 0; i < allDesktops.length; i++) {
-    var d = allDesktops[i];
-    d.wallpaperPlugin = 'org.kde.image';
-    d.currentConfigGroup = ['Wallpaper', 'org.kde.image', 'General'];
-    d.writeConfig('Image', 'file:///home/$USER/Pictures/Wallpapers/picture.jpg');
-}"
+if [ -z "$1" ]; then
+    dbus-send --session --dest=org.kde.plasmashell --type=method_call \
+    /PlasmaShell org.kde.PlasmaShell.evaluateScript \
+    "string:
+    var allDesktops = desktops();
+    for (var i = 0; i < allDesktops.length; i++) {
+        var d = allDesktops[i];
+        d.wallpaperPlugin = 'org.kde.image';
+        d.currentConfigGroup = ['Wallpaper', 'org.kde.image', 'General'];
+        d.writeConfig('Image', '$CURR_IMAGE');
+    }"
 
+else
+    dbus-send --session --dest=org.kde.plasmashell --type=method_call \
+    /PlasmaShell org.kde.PlasmaShell.evaluateScript \
+    "string:
+    var allDesktops = desktops();
+    for (var i = 0; i < allDesktops.length; i++) {
+        var d = allDesktops[i];
+        d.wallpaperPlugin = 'org.kde.image';
+        d.currentConfigGroup = ['Wallpaper', 'org.kde.image', 'General'];
+        d.writeConfig('Image', 'file:///home/ghost/Pictures/Wallpapers/$1');
+    }"
+fi
--- sys/wallpaper_service/on_power.sh
+++ sys/wallpaper_service/on_power.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
+CURR_VIDEO=$(grep -Po '(?<=VideoUrls=)file://.*' ~/.config/plasma-org.kde.plasma.desktop-appletsrc | head -n 1)
+
 export USER_ID=$(id -u)
 export XDG_RUNTIME_DIR="/run/user/$USER_ID"
 export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
 
-dbus-send --session --dest=org.kde.plasmashell --type=method_call \
-/PlasmaShell org.kde.PlasmaShell.evaluateScript \
-"string:
-var allDesktops = desktops();
-for (var i = 0; i < allDesktops.length; i++) {
-    var d = allDesktops[i];
-    d.wallpaperPlugin = 'luisbocanegra.smart.video.wallpaper.reborn';
-    d.currentConfigGroup = ['Wallpaper', 'luisbocanegra.smart.video.wallpaper.reborn', 'General'];
-    d.writeConfig('VideoUrls', 'file:///home/$USER/Pictures/Wallpapers/video.mp4');
-}"
+if [ -z "$1" ]; then
+    dbus-send --session --dest=org.kde.plasmashell --type=method_call \
+    /PlasmaShell org.kde.PlasmaShell.evaluateScript \
+    "string:
+    var allDesktops = desktops();
+    for (var i = 0; i < allDesktops.length; i++) {
+        var d = allDesktops[i];
+        d.wallpaperPlugin = 'luisbocanegra.smart.video.wallpaper.reborn';
+        d.currentConfigGroup = ['Wallpaper', 'luisbocanegra.smart.video.wallpaper.reborn', 'General'];
+        d.writeConfig('VideoUrls', '$CURR_VIDEO');
+        d.writeConfig('LastVideo', '');
+    }"
+
+else 
+    dbus-send --session --dest=org.kde.plasmashell --type=method_call \
+    /PlasmaShell org.kde.PlasmaShell.evaluateScript \
+    "string:
+    var allDesktops = desktops();
+    for (var i = 0; i < allDesktops.length; i++) {
+        var d = allDesktops[i];
+        d.wallpaperPlugin = 'luisbocanegra.smart.video.wallpaper.reborn';
+        d.currentConfigGroup = ['Wallpaper', 'luisbocanegra.smart.video.wallpaper.reborn', 'General'];
+        d.writeConfig('VideoUrls', 'file:///home/ghost/Pictures/Wallpapers/$1');
+        d.writeConfig('LastVideo', '');
+    }"
+fi
